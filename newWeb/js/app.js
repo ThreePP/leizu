bfa49fe29156961914a5735bf7ba2834c64ff145
Judy <judy_zlf@126.com>
2018-01-22 22:28:01 +1100

    用angular-ui-router 实现single page application

---
+++ newWeb/js/app.js
@@ -0,0 +1,55 @@
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  };
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  };
+
+  var djlistchukuState = {
+    name: 'djlist-chuku',
+    url: '/djlist-chuku',
+    templateUrl: 'templates/djlist-chuku.html'
+  };
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+  $stateProvider.state(djlistchukuState);
+
+  
+
+});
+/*
+angular.module('hellogalaxy').component('hello', {
+  template:  '<h3>{{$ctrl.greeting}} Solar System!</h3>' +
+             '<button ng-click="$ctrl.toggleGreeting()">toggle greeting</button>',
+           
+  controller: function() {
+    this.greeting = 'hello';
+  
+    this.toggleGreeting = function() {
+      this.greeting = (this.greeting == 'hello') ? 'whats up' : 'hello'
+    }
+  }
+});
+
+angular.module('hellogalaxy').component('people', {
+  bindings: { people: '<' },
+
+  template: '<h3>Some people:</h3>' +
+            '<ul>' +
+            '  <li ng-repeat="person in $ctrl.people">' +
+            '    <a ui-sref="person({ personId: person.id })">' +
+            '      {{person.name}}' +
+            '    </a>' +
+            '  </li>' +
+            '</ul>'
+});
+*/
