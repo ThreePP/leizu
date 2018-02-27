f7f52d66c755143e2284367e586b6af5ff8e2d22
Judy <judy_zlf@126.com>
2018-02-27 17:19:55 +1100

    改印花生产单样式

--- newWeb/js/controller/yinhua.client.controller.js
+++ newWeb/js/controller/yinhua.client.controller.js
@@ -1,10 +1,21 @@
 //yinhuaController
-myApp.controller('yinhuaController', function($scope, yinhuaService) {
+myApp.controller('yinhuaController', function($scope) {
     $scope.map;
-    $scope.addYinhua = function (map){
-        yinhuaService.addYinhua(map);
+    $scope.formData = {formDate:new Date()};
+    /**预览功能 */
+    $scope.preview =true;
+    $scope.bttnPreview = false;
+    $scope.bttnClose = true;
+    $scope.showPreview = function(){
+
+      $scope.bttnPreview = true;
+      $scope.bttnClose = false;
+      $scope.preview = false;
+
     }
-    $scope.preview = function (){
-        $scope.previewPage = true;
+    $scope.closePreview = function(){
+      $scope.bttnPreview = false;
+      $scope.bttnClose = true;
+      $scope.preview = true;
     }
 });
